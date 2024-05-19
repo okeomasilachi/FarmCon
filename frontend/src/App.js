8cac587d0b2e7e1684b1667d4cad8ea27b514da3
Ogunsanya Taoeeq <taofeeq.muhammad22@gmail.com>
2024-05-19 16:38:19 +0100

    Bootstrap dependency tested and updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,19 @@
-import logo from './logo.svg';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>FarmCon Application</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          welcome to FarmCon Application
         </a>
       </header>
     </div>
