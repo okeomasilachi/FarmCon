70f66522e18cbbce9f211a9366c09997875fac0c
KayDjay <97893126+KayDjay@users.noreply.github.com>
2024-05-31 10:22:45 +0000

    Fixing Feedback crud details

--- backend/controllers/FeedbackController.js
+++ backend/controllers/FeedbackController.js
@@ -1,94 +1,95 @@
-// const sha1 = require("sha1");
+const sha1 = require("sha1");
 
-// const dbClient = require("../utils/db");
-// const redisClient = require("../utils/redis");
-// const { validateRequestData, authUser } = require("../utils/tools");
+const dbClient = require("../utils/db");
+const redisClient = require("../utils/redis");
+const { validateRequestData, authUser } = require("../utils/tools");
 
-// async function postNew(req, res, next) {
-//   const { password, username, email, role } = req.body;
-//   const data = {
-//     password: sha1(password),
-//     username,
-//     email,
-//     role
-//   };
-//   const args = await validateRequestData(data, "users");
-//   if (args) {
-//     return res.status(400).json({ error: args.message }).end();
-//   }
+async function postNewFeedback(req, res, next) {
+  const { userId, productId, rating, comment } = req.body;
+  const data = {
+    userId,
+    productId,
+    rating,
+    comment
+  };
+  const args = await validateRequestData(data, "feedback");
+  if (args) {
+    return res.status(400).json({ error: args.message }).end();
+  }
 
-//   try {
-//     const existingUser = await dbClient.getByCriteria("users", { email });
-//     if (existingUser.length > 0) {
-//       return res.status(409).json({ error: "User already exists" }).end();
-//     }
-//     const user = await dbClient.create("users", data);
-//     return res.status(201).json(user).end();
-//   } catch (err) {
-//     return res.status(400).json({ error: `error creating user [${err}]` }).end();
-//   }
-// }
+  try {
+    const existingFeedback = await dbClient.getByCriteria("feedback", { userId, productId });
+    if (existingFeedback.length > 0) {
+      return res.status(409).json({ error: "Feedback already exists" }).end();
+    }
+    const feedback = await dbClient.create("feedback", data);
+    return res.status(201).json(feedback).end();
+  } catch (err) {
+    return res.status(400).json({ error: `Error creating feedback [${err}]` }).end();
+  }
+}
 
-// async function updateMe(req, res, next) {
-//   const usr = await authUser(req, (data = true));
-//   if (usr) {
-//     const jsonData = req.body;
-//     if (Object.keys(jsonData).length === 0) {
-//       return res.status(400).json({ error: "Empty JSON object" }).end();
-//     }
-//     try {
-//       const user = await dbClient.update("users", usr._id.toString(), jsonData);
-//       return res.status(201).json(user).end();
-//     } catch (err) {
-//       return res.status(400).json({ error: "Error updating user" }).end();
-//     }
-//   } else {
-//     return res.status(401).json({ error: "Unauthorized" });
-//   }
-// }
+async function updateFeedback(req, res, next) {
+  const usr = await authUser(req, true);
+  if (usr) {
+    const feedbackId = req.params.id;
+    const jsonData = req.body;
+    if (Object.keys(jsonData).length === 0) {
+      return res.status(400).json({ error: "Empty JSON object" }).end();
+    }
+    try {
+      const feedback = await dbClient.update("feedback", feedbackId, jsonData);
+      return res.status(200).json(feedback).end();
+    } catch (err) {
+      return res.status(400).json({ error: "Error updating feedback" }).end();
+    }
+  } else {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+}
 
-// async function deleteMe(req, res, next) {
-//   const usr = await authUser(req, (data = true));
-//   if (usr) {
-//     try {
-//       const user = await dbClient.delete("users", usr._id);
-//       return res.status(200).json({}).end();
-//     } catch (err) {
-//       return res.status(400).json({ error: "Error deleting user" }).end();
-//     }
-//   } else {
-//     return res.status(401).json({ error: "Unauthorized" });
-//   }
-// }
+async function deleteFeedback(req, res, next) {
+  const usr = await authUser(req, true);
+  if (usr) {
+    const feedbackId = req.params.id;
+    try {
+      const feedback = await dbClient.delete("feedback", feedbackId);
+      return res.status(200).json({}).end();
+    } catch (err) {
+      return res.status(400).json({ error: "Error deleting feedback" }).end();
+    }
+  } else {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+}
 
-// async function getMe(req, res) {
-//   const usr = await authUser(req, (data = true));
-//   if (usr) {
-//     try {
-//       const { _id, email } = usr;
-//       return res.status(200).json({ _id, email }).end();
-//     } catch (error) {
-//       return res.status(401).json({ error: "Unauthorized" }).end();
-//     }
-//   } else {
-//     return res.status(401).json({ error: "Unauthorized" }).end();
-//   }
-// }
+async function getFeedback(req, res) {
+  const feedbackId = req.params.id;
+  try {
+    const feedback = await dbClient.getById("feedback", feedbackId);
+    if (feedback) {
+      return res.status(200).json(feedback).end();
+    } else {
+      return res.status(404).json({ error: "Feedback not found" }).end();
+    }
+  } catch (error) {
+    return res.status(500).json({ error: "Server error" }).end();
+  }
+}
 
-// async function getAll(req, res, next) {
-//   try {
-//     const data = await dbClient.getAll("users");
-//     const users = data.map(({ password, _id, ...rest }) => rest);
-//     return res.status(200).json(users).end();
-//   } catch (err) {
-//     return res.status(500).json({ error: "server error" }).end();
-//   }
-// }
+async function getAllFeedback(req, res, next) {
+  try {
+    const feedbacks = await dbClient.getAll("feedback");
+    return res.status(200).json(feedbacks).end();
+  } catch (err) {
+    return res.status(500).json({ error: "Server error" }).end();
+  }
+}
 
-// module.exports = {
-//   postNew,
-//   getMe,
-//   deleteMe,
-//   updateMe,
-//   getAll,
-// };
+module.exports = {
+  postNewFeedback,
+  getFeedback,
+  deleteFeedback,
+  updateFeedback,
+  getAllFeedback,
+};
