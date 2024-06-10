aaf6763a737c2d5514a999a6a3833eaebb390ff6
tsuccess <taofeeq.muhammad22@gmail.com>
2024-06-10 02:48:21 +0100

    URL access invalid

--- frontend/src/pages/User.jsx
+++ frontend/src/pages/User.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import Sidebar from '../components/sidebar/Sidebar'
 import Userstable from '../components/user/Userstable'
+import { useNavigate } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import { userInfo } from "../atoms/User";
 
 const User = () => {
-  
+  let user = useRecoilValue(userInfo);
+  let redir = useNavigate();
+
+  useEffect(()=> {
+    if (!user.isLoggedIn) {
+      redir("../");
+    }
+  }, [user.isLoggedIn, redir])
   return (
 	<div className="container-fluid">
       <div className="row">
